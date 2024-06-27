43ad9bfa773d27c75e3bce4b7419bcabdad7414e
steve <steve@DESKTOP-8OHDCI7>
2024-06-27 12:41:05 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Assignment_7._4_Car_Parking_Garage_Simulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+        }
+    }
+    public class ParkingSystem
+    {
+        int[] slots = new int[4];
+        public ParkingSystem(int big, int medium, int small)
+        {
+            slots[0] = 0;
+            slots[1] = big;
+            slots[2] = medium;
+            slots[3] = small;
+
+        }
+
+        public bool AddCar(int carType)
+        {
+            if (slots[carType] > 0)
+            {
+                slots[carType]--;
+                return true;
+            }
+            return false;
+        }
+    }
+}
